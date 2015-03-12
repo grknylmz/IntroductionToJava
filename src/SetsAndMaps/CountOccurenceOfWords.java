31f48ccaca3b8a19c08ab41db6b5ede1503a78f2
Gurkan Yilmaz <yilmaz.gurkan@hotmail.com.tr>
2015-03-12 14:45:28 +0200

    Codes

--- src/SetsAndMaps/CountOccurenceOfWords.java
+++ src/SetsAndMaps/CountOccurenceOfWords.java
@@ -1,10 +1,38 @@
 package SetsAndMaps;
 
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
 /**
  * Created by Gurkan on 12.03.2015.
  */
 public class CountOccurenceOfWords {
     public static void main(String[] args) {
-        String
+        String text = "Good morning. Have a good class." + "Have a good visit. Have Fun!";
+        Map<String , Integer> map = new TreeMap<>();
+
+        String[] words = text.split("[ \n\t\r.,;:!?(){]");
+        for (int i = 0; i < words.length; i++) {
+            String key = words[i].toLowerCase();
+
+            if(key.length()>0){
+                if(map.containsKey(key))
+                    map.put(key,1);
+            }
+            else {
+                int value = map.get(key);
+                value++ ;
+                map.put(key,value);
+
+            }
+
+
+            Set<Map.Entry<String,Integer>> entrySet = map.entrySet();
+            for(Map.Entry<String , Integer> entry : entrySet)
+                System.out.println(entry.getValue() + "\t" + entry.getKey());
+
+
+        }
     }
 }
