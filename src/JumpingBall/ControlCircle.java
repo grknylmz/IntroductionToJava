bd1d769b319c8b9f321b6928dd1e78602a590c16
Gurkan Yilmaz <yilmaz.gurkan@hotmail.com.tr>
2015-03-18 00:08:41 +0200

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	src/SetsAndMaps/CountOccurrenceOfWords.java

--- src/JumpingBall/ControlCircle.java
+++ src/JumpingBall/ControlCircle.java
@@ -1,3 +1,5 @@
+package JumpingBall;
+
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
--- src/JumpingBall/FontDemo.java
+++ src/JumpingBall/FontDemo.java
@@ -1,3 +1,5 @@
+package JumpingBall;
+
 import javafx.application.Application;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
