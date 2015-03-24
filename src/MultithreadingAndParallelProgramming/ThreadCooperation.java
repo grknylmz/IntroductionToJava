9c5bc8df68216249e8ff6a96baae73e377a673c2
Gurkan Yilmaz <yilmaz.gurkan@hotmail.com.tr>
2015-03-24 22:12:18 +0200

    impFiles

---
+++ src/MultithreadingAndParallelProgramming/ThreadCooperation.java
@@ -0,0 +1,85 @@
+package MultithreadingAndParallelProgramming;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Created by Gürkan on 24.3.2015.
+ */
+public class ThreadCooperation {
+    private static Account account = new Account();
+
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+    }
+
+
+    public static class DepositTask implements Runnable{
+        @Override
+        public void run(){
+            try{
+                account.deposit((int) (Math.random()*10)+1);
+                Thread.sleep(1000);
+            }
+            catch (InterruptedException ex){
+                ex.printStackTrace();
+            }
+        }
+    }
+    public static class WithdrawTask implements Runnable{
+        @Override
+        public void run(){
+            while (true){
+                account.withdraw((int )(Math.random()*10)+1);
+            }
+        }
+    }
+
+
+    private static class Account {
+
+        private static Lock lock = new ReentrantLock();
+
+        private static Condition newDeposit = lock.newCondition();
+
+        private int balance = 0;
+
+        public int getBalance() {
+            return balance;
+        }
+
+
+        public void withdraw(int amount) {
+            try {
+                while (balance < amount) {
+                    System.out.println("\t\t\t Wait for deposit : ");
+                    newDeposit.wait();
+                }
+                balance -= amount;
+                System.out.println("\t\t\tWithdraw " + amount + "\t\t" + getBalance());
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+
+            } finally {
+                lock.lock();
+            }
+        }
+
+        public void deposit(int amount) {
+            lock.lock();
+
+            try {
+                balance += amount;
+                System.out.println("Deposit " + amount + "\t\t\t\t" + getBalance());
+                newDeposit.signalAll();
+            }
+            finally {
+                lock.unlock();
+            }
+
+        }
+    }
+}
